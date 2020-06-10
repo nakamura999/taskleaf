f0c616e95787465dd590ea07ab42903895441201
nakamura999 <masahiro9ama9@gmail.com>
2020-06-10 18:57:09 +0900

    バリデーション追加（オリジナル検証）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 30 }
+	validates :description, presence: true
+
+	validate :validate_name_not_comma
+
+	private
+
+	def validate_name_not_comma
+		errors.add(:name, "にカンマを含めることはできません") if name&.include?(',')
+	end
+	# オリジナル検証コード
+
 end
