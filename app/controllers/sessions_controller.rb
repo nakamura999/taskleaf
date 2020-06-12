656360fca0349854581f3f12f9842b5b05afc4e2
nakamura999 <masahiro9ama9@gmail.com>
2020-06-12 17:32:08 +0900

    seesionsコントローラーアクション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	user = User.find_by(email: session_params[:email])
+  	if user&.authenticate(session_params[:password])
+  	   # authenticateメソッドでパスワードによる認証を行う　has_secure_passwordによる記述で自動的に追加されたメソッド
+  		session[:user_id] = user.id
+  		redirect_to root_path, notice: "ログインしました"
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def session_params
+  	params.require(:session).permit(:email, :password)
+  end
+
 end
