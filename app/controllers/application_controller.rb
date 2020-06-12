13b7762e4db3a6bfca3ff0a5e0a2ef4ca441a73e
nakamura999 <masahiro9ama9@gmail.com>
2020-06-12 18:07:17 +0900

    ログイン機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 	helper_method :current_user
 	# すべてのviewからcurrent_userが使える
+	before_action :login_required
 
 	private
 
 	def current_user
 		@cuurent_user || = User.find_by(id: session[:user_id]) if session[:user_id]
 		# session[:user_id]にユーザーのidが入り、current_userでUserオブジェクトを取得できる
+	end
+
+	def login_required
+		redirect_to login_url unless current_user
 	end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+  # 親クラスに定義してある　login_required　を通らないようにする
+
   def new
   end
 
