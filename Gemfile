49880f18a44a436221784a027bf12f13dde08ec3
nakamura999 <masahiro9ama9@gmail.com>
2020-06-13 20:25:40 +0900

    タスクテスト作成

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'capybara', '>= 2.15'
   gem 'rspec-rails', '~> 3.7'
   gem 'factory_bot_rails', '~> 4.11'
 end
@@ -54,7 +55,6 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+	describe '一覧表示機能' do
+		# ユーザーを作成
+		let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+		let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
+		before do
+			# ユーザーAでログインする
+			FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+			visit login_path
+			fill_in 'session[email]', with: login_user.email
+			fill_in 'session[password]', with: login_user.password
+			click_button 'ログイン'
+		end
+
+		context 'ユーザーAがログインしている時' do
+			let(:login_user) { user_a }
+
+			it 'ユーザーAが作成したタスクが表示される' do
+				# 作成済のタスクの名称が画面上に表示されていることを確認
+				expect(page).to have_content '最初のタスク'
+			end
+		end
+
+		context 'ユーザーBがログインしている時' do
+			let(:login_user) { user_b }
+
+			it 'ユーザーAが作成したタスクが表示されない' do
+				# ユーザーAが作成したタスクの名称が画面上に表示されない
+				expect(page).to have_no_content '最初のタスク'
+			end
+		end
+	end
+end
