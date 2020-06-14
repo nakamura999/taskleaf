50edc946323958882139e85f90b4f59014258f05
nakamura999 <masahiro9ama9@gmail.com>
2020-06-14 14:56:32 +0900

    タスクテスト変更

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
-	describe '一覧表示機能' do
 		# ユーザーを作成
 		let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
 		let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+		let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
 		before do
 			# ユーザーAでログインする
@@ -14,13 +14,17 @@ describe 'タスク管理機能', type: :system do
 			fill_in 'session[password]', with: login_user.password
 			click_button 'ログイン'
 		end
+        
+        # 共通部分　it_behaves_like
+		shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+			it { expect(page).to have_content '最初のタスク' }
+		end
 
-		context 'ユーザーAがログインしている時' do
-			let(:login_user) { user_a }
+        describe '一覧機能' do
+			context 'ユーザーAがログインしている時' do
+				let(:login_user) { user_a }
 
-			it 'ユーザーAが作成したタスクが表示される' do
-				# 作成済のタスクの名称が画面上に表示されていることを確認
-				expect(page).to have_content '最初のタスク'
+				it_behaves_like 'ユーザーAが作成したタスクが表示される'
 			end
 		end
 
@@ -32,5 +36,16 @@ describe 'タスク管理機能', type: :system do
 				expect(page).to have_no_content '最初のタスク'
 			end
 		end
-	end
+
+		describe '詳細表示機能' do
+			context 'ユーザーAがログインしている時' do
+				let(:login_user) { user_a }
+
+				before do
+					visit task_path(task_a)
+				end
+
+				it_behaves_like 'ユーザーAが作成したタスクが表示される'
+			end
+		end
 end
